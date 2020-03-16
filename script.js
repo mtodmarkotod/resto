853583893c369101648beb13071ef4f66169a67d
marko.todorovic <mtod.markotod@gmail.com>
2020-03-16 17:09:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,138 @@
+$(document).ready(function(){
+
+   $('.nav-item').click(function(){
+       $(this).siblings().removeClass('active-nav');
+       $(this).addClass('active-nav');
+   });
+
+   $('.view-all-btn').click(function(){
+     $('body').addClass('disable-scroll');  
+     //$('body').css('padding-bottom', '100px'); 
+   });
+
+   $('.close').click(function(){
+    $('body').removeClass('disable-scroll');   
+  });
+
+// SLIDER ***********
+   $('.slider').each(function() {
+    var $this = $(this);
+    var $group = $this.find('.slide_group');
+    var $slides = $this.find('.slide');
+    var bulletArray = [];
+    var currentIndex = 0;
+    var timeout;
+    
+    function move(newIndex) {
+      var animateLeft, slideLeft;
+      
+      advance();
+      
+      if ($group.is(':animated') || currentIndex === newIndex) {
+        return;
+      }
+      
+      bulletArray[currentIndex].removeClass('active');
+      bulletArray[newIndex].addClass('active');
+      
+      if (newIndex > currentIndex) {
+        slideLeft = '100%';
+        animateLeft = '-100%';
+      } else {
+        slideLeft = '-100%';
+        animateLeft = '100%';
+      }
+      
+      $slides.eq(newIndex).css({
+        display: 'block',
+        left: slideLeft
+      });
+      $group.animate({
+        left: animateLeft
+      }, function() {
+        $slides.eq(currentIndex).css({
+          display: 'none'
+        });
+        $slides.eq(newIndex).css({
+          left: 0
+        });
+        $group.css({
+          left: 0
+        });
+        currentIndex = newIndex;
+      });
+    }
+    
+    function advance() {
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        if (currentIndex < ($slides.length - 1)) {
+          move(currentIndex + 1);
+        } else {
+          move(0);
+        }
+      }, 4000);
+    }
+    
+    $('.next_btn').on('click', function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    });
+    
+    $('.previous_btn').on('click', function() {
+      if (currentIndex !== 0) {
+        move(currentIndex - 1);
+      } else {
+        move(3);
+      }
+    });
+    
+    $.each($slides, function(index) {
+      var $button = $('<a class="slide_btn">&bull;</a>');
+      
+      if (index === currentIndex) {
+        $button.addClass('active');
+      }
+      $button.on('click', function() {
+        move(index);
+      }).appendTo('.slide_buttons');
+      bulletArray.push($button);
+    });
+    
+    advance();
+  });
+
+
+
+  });
+
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
