83b4e24c89eddcf105f1a5cb101727ee680e6bc0
marko.todorovic <mtod.markotod@gmail.com>
2020-03-18 13:00:20 +0100

    need full width

--- script.js
+++ script.js
@@ -117,7 +117,36 @@ $(document).ready(function(){
       speed:3000,
       arrows:false,
       dots:true,
-      appendDots:$('.section-heading-box')
+      appendDots:$('.section-heading-box'),
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 
 
