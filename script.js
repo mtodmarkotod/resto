393c8547561be1a2549000bf8e4f5960636b857b
marko.todorovic <mtod.markotod@gmail.com>
2020-03-23 16:54:26 +0100

    section gallery finished

--- script.js
+++ script.js
@@ -109,15 +109,16 @@ $(document).ready(function(){
 
   //SLICK SLIDER
   
-    $('.slick').slick({
+    $('.featured-dishes-slider').slick({
       slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+      variableWidth:true,
       speed:3000,
       arrows:false,
       dots:true,
-      appendDots:$('.section-heading-box'),
+      appendDots:$('.add-dots'),
 
       responsive: [
         {
@@ -150,9 +151,62 @@ $(document).ready(function(){
     });
 
 
+    $('.heart').each(function(){
+      $(this).on('click', function() {
+          var count = parseInt($(this).data('click'), 10) || 0;
+          $(this).css({'fill': '#F34949', 'stroke': '#F34949'});
+          count++;
+          $(this).data('click', count);
+          $(this).next().html('<span>' + count + ' people love this</span>');
+      });
   });
 
 
+
+});
+
+//SMOOTH SCROLL
+
+ // Select all links with hashes
+ $('a[href*="#"]')
+ // Remove links that don't actually link to anything
+ .not('[href="#"]')
+ .not('[href="#0"]')
+ .click(function(event) {
+ // On-page links
+ if (
+     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+     && 
+     location.hostname == this.hostname
+ ) {
+     // Figure out element to scroll to
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     // Does a scroll target exist?
+     if (target.length) {
+     // Only prevent default if animation is actually gonna happen
+     event.preventDefault();
+     $('html, body').animate({
+         scrollTop: target.offset().top
+     }, 1000, function() {
+         // Callback after animation
+         // Must change focus!
+         var $target = $(target);
+         $target.focus();
+         if ($target.is(":focus")) { // Checking if the target was focused
+         $target.css('outline', '0');
+         return false;
+         } else {
+         $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+         $target.focus(); // Set focus again
+         };
+     });
+     }
+ }
+ });
+
+
+//MODAL
 
   // Get the modal
 var modal = document.getElementById("myModal");
